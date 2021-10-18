2b42d93d62c25ccff9080479acad7a527ad00b56
Raven Thevar <raven_genius@live.com>
2021-10-18 14:28:26 +0800

    API CALL - for everyone

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App (){
+
+React.useEffect(() => {
+
+  const getData = async () => {
+
+    // Below is to call API
+    const res =  await fetch("https://www.superheroapi.com/api.php/4917344464945186/69/connections")
+    const test_data = await res.json()
+
+  // below is to specify which data from JSON you would like to call, such as, name, id.
+  console.log(test_data)
+  }
+
+  const data = getData()
+});
+
+return(
+  <div></div>
+)
+
 }
 
 export default App;
